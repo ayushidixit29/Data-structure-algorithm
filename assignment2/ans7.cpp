90ffdb2cea12535768cc6eac35894b053f91a325
ayushidixit29 <104644332+ayushidixit29@users.noreply.github.com>
2022-05-06 17:47:26 +0530

    Update ans7.cpp

--- assignment2/ans7.cpp
+++ assignment2/ans7.cpp
@@ -1,29 +1,27 @@
-#include<bits/stdc++.h>
+ #include<bits/stdc++.h>
 using namespace std;
 
 bool isPrime(int n){
-     if(n==1){
-        return 0;
+    if(n<=1){
+       return 0; 
     }
-    else{
-        for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0){
-                return 0;
-            }
-            else{
-                return 1;
-            }
+    else
+    for (int i=2;i<=sqrt(n);i++){
+      if (n%i==0){
+            return 0;
         }
     }
     return 1;
+    
 }
 int main() {
-    for(int i=1;i<30;i++){
-         if (isPrime(i)){
-            cout<<"the number "<<i<<" is prime\n";
-         }
-         else{
-            cout<<"the number "<<i<<" is not prime\n";
-         }   
+    int p;
+    cin>>p;
+    if (isPrime(p)==0){
+        cout<<"not prime";
+        
+    }
+    else {
+        cout<<"prime";
     }
 }
