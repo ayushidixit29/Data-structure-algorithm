5aa0832bffb567cbaa0b4e6f23368d0ba4aec8a0
ayushidixit29 <104644332+ayushidixit29@users.noreply.github.com>
2022-06-14 23:25:41 +0530

    Create Sqrt(x).cpp

---
+++ assignment 10/Sqrt(x).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        if(x <= 1)
+            return x;
+        return (sqrt(x));
+        
+    }
+    void main()
+    {
+        int x=7;
+        cout << mySqrt(x) << '\n';
+    }
+};
