4a2b1cdd31030309a9e8048956cbbb1f970d597a
ayushidixit29 <104644332+ayushidixit29@users.noreply.github.com>
2022-07-21 11:15:16 +0530

    Create stock price.cpp

---
+++ assignment30/stock price.cpp
@@ -0,0 +1,42 @@
+class StockPrice {
+public:
+    
+    unordered_map<int,int>mp; //timestamp as key and price as value
+    int currentTime = 0;
+    multiset<int>S; //insert price
+    StockPrice() {
+        
+    }
+    
+    void update(int timestamp, int price) {
+        if(mp.find(timestamp) != mp.end()){
+            auto it = S.find(mp[timestamp]);
+	        S.erase(it);
+        }
+        mp[timestamp]=price;
+        S.insert(price);
+        currentTime = max(timestamp, currentTime);
+    }
+    
+    int current() {
+        return mp[currentTime];
+    }
+    
+    int maximum() {
+        auto it = S.end();
+        return  *(--it);
+    }
+    
+    int minimum() {
+        return *S.begin();
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
