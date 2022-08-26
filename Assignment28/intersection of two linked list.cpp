475c34c4228ebbd56ee2046749f4f0e2ed472a5e
ayushidixit29 <104644332+ayushidixit29@users.noreply.github.com>
2022-08-26 12:21:22 +0530

    Create intersection of two linked list.cpp

---
+++ Assignment28/intersection of two linked list.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+       ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+	{
+		unordered_set<ListNode *>S;
+          while(headA){
+              S.insert(headA);
+              headA =headA->next;
+          }
+           while(headB){
+               if(S.find(headB)!=S.end()){
+                   return headB;
+               }
+               headB =headB->next;
+           }
+           return headB;
+       }
+};
