cab1bc295db69a044fed03570083c93d46108b84
ayushidixit29 <104644332+ayushidixit29@users.noreply.github.com>
2023-02-24 13:57:20 +0530

    Create single number.cpp

---
+++ single number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = 0; i < n - 1; i++)
+            arr[i + 1] = arr[i] ^ arr[i + 1];
+        return arr[n- 1]; 
+    }
+};
+
